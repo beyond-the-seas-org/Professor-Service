3a35487de4aca6dd506e98995beb221779a2d7f2
ANIK115 <aniknj4@gmail.com>
2023-08-20 19:57:02 +0600

    all professors short details api is complete

--- professors/apis/Get_Professor_Short_Details.py
+++ professors/apis/Get_Professor_Short_Details.py
@@ -10,6 +10,7 @@ import requests
 from professors.models.professor import *
 from professors.models.field import *
 from professors.models.university_ranks import *
+from professors.models.professor_area_of_interests import *
 
 #this class is for getting all professors from database
 
@@ -24,15 +25,33 @@ class Get_All_professor_short_details(Resource):
 
             print(all_professors_short_details)
 
+            
+
             #create json format
             all_professors_short_details_json = []
             for professor in all_professors_short_details:
+
+                #get area of interest ids for each professor
+                area_of_interest_ids = db.session.query(ProfessorAreaOfInterestModel.area_of_interest_id).filter(ProfessorAreaOfInterestModel.professor_id == professor[0].id).all()
+                #filter the commas from the list
+                area_of_interest_ids = [item for t in area_of_interest_ids for item in t]
+                print(area_of_interest_ids)
+
+                #get field names for each area of interest
+                
+                field_names = db.session.query(FieldModel.name).filter(FieldModel.id.in_(area_of_interest_ids)).all()
+                #filter the commas from the list
+                field_names = [item for t in field_names for item in t]
+                print(field_names)
+
                 all_professors_short_details_json.append({
                     "id":professor[0].id,
                     "name":professor[0].name,
+                    "email":professor[0].email,
                     "university_id":professor[0].university_id,
                     "university_name":professor[1].name,
-                    "university_rank":professor[1].rank
+                    "university_rank":professor[1].rank,
+                    "field_names":field_names
                 })
             
 
